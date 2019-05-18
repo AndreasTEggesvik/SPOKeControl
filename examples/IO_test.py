866cd88ec7c85cd42ce1ff350c276f637b7d097f
Andreas <andreas.t.eggesvik@hotmail.com>
2019-05-18 14:08:19 +0200

    before merge

--- examples/IO_test.py
+++ examples/IO_test.py
@@ -16,7 +16,7 @@ while (1):
 #		encoder_instance.update_counter(1)
 #		encoder_instance.update_counter(2)
 		if (not i % 10):
-			print("Counter 1:", geo.rad2r2(encoder_instance.read_counter_rad(1, plc_handler)), "(", encoder_instance.read_counter_rad(1, plc_handler), ")",  " |  Counter 2:", geo.rad2theta4(encoder_instance.read_counter_rad(2, plc_handler)))
+			print("Counter 1:", geo.rad2r2(encoder_instance.read_counter_rad(1, plc_handler)), "(", encoder_instance.read_counter_deg(1, plc_handler), ")",  " |  Counter 2:", geo.rad2theta4(encoder_instance.read_counter_rad(2, plc_handler)))
 		time.sleep(0.1)
-	encoder_instance.reset_counter(1)
-	encoder_instance.reset_counter(2)
+	encoder_instance.reset_counter(1, plc_handler)
+	encoder_instance.reset_counter(2, plc_handler)

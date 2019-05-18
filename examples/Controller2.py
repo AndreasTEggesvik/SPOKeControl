279a5790865b79e8cfd5261a43349cf5346bd0ae
Andreas T. Eggesvik <andreas.t.eggesvik@hotmail.com>
2019-05-18 14:12:26 +0200

    added ticks

--- examples/Controller2.py
+++ examples/Controller2.py
@@ -130,6 +130,7 @@ def main(graphPipe, graphPipeReceiver, buttonPipe, graphPipeSize, graphLock, sto
 			if (i == 15):
 				print("r2 value = ", control_instance.r2, " | theta4 value = ", control_instance.theta4)
 				print("Degrees: ", control_instance.encoder_instance.read_counter_deg(1, control_instance.plc_handler))
+				print("Ticks: ", control_instance.encoder_instance.readCounterValue(1, control_instance.plc_handler))
 				if (graphPipeSize.value == 0):
 					control_instance.eraseBufferData()
 				control_instance.bufferData()

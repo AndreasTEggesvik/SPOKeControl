87a3ec2aec945df73555425610726a36d61cc404
Andreas T. Eggesvik <andreas.t.eggesvik@hotmail.com>
2019-05-24 20:22:58 +0200

    Added update as function

--- examples/Controller3.py
+++ examples/Controller3.py
@@ -9,6 +9,7 @@ import collections
 import Geometry
 from multiprocessing import Process, Pipe, Value, Lock
 import SPOKe_IO
+import pymonarco_hat as plc
 
 
 def main(graphPipe, graphPipeReceiver, buttonPipe, graphPipeSize, graphLock, stopButtonPressed, newButtonData, operatingTimeConstant):
@@ -20,8 +21,9 @@ def main(graphPipe, graphPipeReceiver, buttonPipe, graphPipeSize, graphLock, sto
 #	while ((not control_instance.timeout) and (stopButtonPressed.value == 0)): # and (not control_instance.isStuck())):# and (not control_instance.ls_instance.anyActive())): # and control_instance.theta4_e > 0.017 and control_instance.r2_e > 0.02): 
 		# Only check time when testing while the trajectory is still moving, theta4_e < 1 deg, r2_e < 2 cm.
 	while(1):
-		a = control_instance.encoder_instance.read_counter_deg(1, control_instance.plc_handler)
+		
 #		control_instance.encoder_instance.update_counter(1, control_instance.plc_handler)
+		control_instance.updatePosition()
 		if (i == 9):
 			#print("r2 value = ", control_instance.r2, " | theta4 value = ", control_instance.theta4)
 			print("Degrees: ", control_instance.encoder_instance.read_counter_deg(1, control_instance.plc_handler), ' ( ', control_instance.encoder_instance.local_counter1, ')' )
@@ -30,8 +32,6 @@ def main(graphPipe, graphPipeReceiver, buttonPipe, graphPipeSize, graphLock, sto
 		i += 1
 		time.sleep(0.02)
 
-import pymonarco_hat as plc
-
 class Controller:
 	def __init__(self):
 		lib_path = '../../../pymonarco-hat/monarco-c/libmonarco.so'
@@ -39,3 +39,6 @@ class Controller:
 		self.encoder_instance = SPOKe_IO.Encoder_input(self.plc_handler)
 		self.motor_control = SPOKe_IO.Motor_output(self.plc_handler)
 		self.ls_instance = SPOKe_IO.LimitSwitch()			
+
+	def updatePosition(self):
+		self.a = self.encoder_instance.read_counter_deg(1, self.plc_handler)

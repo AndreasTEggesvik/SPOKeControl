b0a214877fb986363699f52ae33cc4155949551b
Andreas <andreas.t.eggesvik@hotmail.com>
2019-05-24 20:17:53 +0200

    This version is working

--- examples/Controller3.py
+++ examples/Controller3.py
@@ -11,8 +11,8 @@ from multiprocessing import Process, Pipe, Value, Lock
 import SPOKe_IO
 
 
-#def main(graphPipe, graphPipeReceiver, buttonPipe, graphPipeSize, graphLock, stopButtonPressed, newButtonData, operatingTimeConstant):
-def main():
+def main(graphPipe, graphPipeReceiver, buttonPipe, graphPipeSize, graphLock, stopButtonPressed, newButtonData, operatingTimeConstant):
+#def main():
 	# Initializing the robot, guaranteeing a safe starting position
 	control_instance = Controller()
 
@@ -20,8 +20,9 @@ def main():
 #	while ((not control_instance.timeout) and (stopButtonPressed.value == 0)): # and (not control_instance.isStuck())):# and (not control_instance.ls_instance.anyActive())): # and control_instance.theta4_e > 0.017 and control_instance.r2_e > 0.02): 
 		# Only check time when testing while the trajectory is still moving, theta4_e < 1 deg, r2_e < 2 cm.
 	while(1):
+		a = control_instance.encoder_instance.read_counter_deg(1, control_instance.plc_handler)
 #		control_instance.encoder_instance.update_counter(1, control_instance.plc_handler)
-		if (i == 15):
+		if (i == 9):
 			#print("r2 value = ", control_instance.r2, " | theta4 value = ", control_instance.theta4)
 			print("Degrees: ", control_instance.encoder_instance.read_counter_deg(1, control_instance.plc_handler), ' ( ', control_instance.encoder_instance.local_counter1, ')' )
 
@@ -34,7 +35,7 @@ import pymonarco_hat as plc
 class Controller:
 	def __init__(self):
 		lib_path = '../../../pymonarco-hat/monarco-c/libmonarco.so'
-		self.plc_handler = plc.Monarco(lib_path, debug_flag=plc.MONARCO_DPF_WRITE | plc.MONARCO_DPF_WARNING)	
+		self.plc_handler = plc.Monarco(lib_path, debug_flag=plc.MONARCO_DPF_WRITE)
 		self.encoder_instance = SPOKe_IO.Encoder_input(self.plc_handler)
 		self.motor_control = SPOKe_IO.Motor_output(self.plc_handler)
 		self.ls_instance = SPOKe_IO.LimitSwitch()			
